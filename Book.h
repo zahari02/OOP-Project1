a3ddeaf5dc93745cd673803aa8fbb346c2dec208
zahari02 <35264537+zahari02@users.noreply.github.com>
2022-04-28 11:58:48 +0300

    Add files via upload

--- Book.h
+++ Book.h
@@ -1,5 +1,17 @@
 class Book
 {
-    char *author,*heading,*directory,*description,*rating,*isbn;
+    char *author,*heading,*directory,*description,*isbn;
+    int rating
 
+    void deleteDyn();
+
+public:
+    Book();
+    Book(const Book&);
+    Book(Book&&);
+    Book& operator=(const Book&);
+    Book& operator=(Book&&);
+    ~Book();
+
+    bool load();
 };
